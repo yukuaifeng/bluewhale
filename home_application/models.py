c69c48c1f39c056fb3fa56b0c885e97ca6b32d3b
yukuaifeng <yukuaifeng@hotmail.com>
2019-06-11 16:29:03 +0800

    作业三第一次提交

--- home_application/models.py
+++ home_application/models.py
@@ -1,4 +1,37 @@
 # -*- coding: utf-8 -*-
-# from django.db import models
+from django.db import models
+import datetime
 
 # Create your models here.
+
+class HostModelManager(models.Manager):
+    def get_record(self, data):
+        try:
+            qs = super().get_queryset()
+            res_dict = [{
+                'host_name' = item.host_name,
+                'host_ip' = item.host_ip,
+                'host_os' = item.host_os,
+                'host_partition' = item.host_partition,
+                'create_time' = item.create_time
+            }]
+        except Exception,e:
+            print("获取失败", e)
+        return res_dict
+
+
+
+class HostModel(models.Model):
+    os_choices = (
+        ('windows','windows'),
+        ('linux', 'linux'),
+        ('mac','mac'),
+    )
+    host_name = models.CharField("主机名", max_length=64)
+    host_ip = models.GenericIPAddressField("主机ip", max_length=64)
+    host_os = models.CharField("操作系统", choices = os_choices, max_length=64)
+    host_partition = models.CharField("磁盘分区", max_length=64)
+    create_time = models.DateTimeField("创建时间", auto_now=True)
+
+    objects = HostModelManager()
+    
--- home_application/views.py
+++ home_application/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import HostModel
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
@@ -18,3 +19,34 @@ def test(request):
 
 def hello(request):
     return render(request, 'home_application/helloworld.html')
+
+def host(request):
+    return render(request, 'home_application/hostmanage.html')
+
+def host_data(request):
+    if request.method == 'POST':
+        host_name = request.POST.get("host_name", None)
+        host_ip = request.POST.get("host_ip", None)
+        host_os = request.POST.get("host_os", None)
+        host_partition = request.POST.get("host_partition", None)
+
+        if "" in [host_name, host_ip, host_os, host_partition]:
+            return render_json('code':-1, 'message':'lost someone')
+        
+        try:
+            HostModel.objects.create(
+                host_name = host_name, host_ip = host_ip, host_os = host_os, host_partition = host_partition)
+        except Exception as e:
+            return render_json({'code':-1, 'message':'something wrong'})
+        
+        return render_json({'code':0, 'message':'insert success'})
+    else:
+        return render_json({'code':0, 'items' : HostModel.objects.to_dict(),
+                            'catalogues':{
+                                'hostname':'主机名',
+                                'hostip' : '主机ip',
+                                'hostos' : '主机操作系统',
+                                'hostpartition' : '磁盘分区',
+                                'createtime' : '创建时间'
+                            }})
+
